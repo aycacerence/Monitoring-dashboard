bdafa677f46c8a0853eb79b3fcdd953f428627b7
Sevval Ayca Cerence <aycacerence@gmail.com>
2026-06-30 13:38:18 +0300

    feat: dummy veri katmanının (mock JSON) oluşturulması ve tema konfigürasyonu

---
+++ src/data/index.js
@@ -0,0 +1,21 @@
+import kpiData from './kpiData.json';
+import cpuUsageData from './cpuUsageData.json';
+import networkTrafficData from './networkTrafficData.json';
+import deviceStatusDistribution from './deviceStatusDistribution.json';
+import alertsData from './alertsData.json';
+import systemSummaryData from './systemSummaryData.json';
+import resourceUsageData from './resourceUsageData.json';
+import devicesData from './devicesData.json';
+import themeData from './themeData.json';
+
+export {
+  kpiData,
+  cpuUsageData,
+  networkTrafficData,
+  deviceStatusDistribution,
+  alertsData,
+  systemSummaryData,
+  resourceUsageData,
+  devicesData,
+  themeData,
+};
