a852602f323b48a7aa4ea40002e1bd4480e5c325
Sevval Ayca Cerence <aycacerence@gmail.com>
2026-07-07 00:35:06 +0300

    fix:web panel ayarları varsayılan görünümü iyileştirme

--- src/components/kpi/KpiGrid/KpiGrid.jsx
+++ src/components/kpi/KpiGrid/KpiGrid.jsx
@@ -104,7 +104,7 @@ function KpiGrid() {
 
   if (showLoadingState) {
     return (
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:h-full lg:grid-cols-12">
+      <div className="grid grid-cols-1 gap-4 transition-opacity duration-500 ease-in opacity-100 sm:grid-cols-2 md:grid-cols-3 lg:h-full lg:grid-cols-12">
         {[...Array(6)].map((_, index) => (
           <div key={index} className="lg:col-span-2 lg:min-h-0">
             <KpiCardSkeleton />
